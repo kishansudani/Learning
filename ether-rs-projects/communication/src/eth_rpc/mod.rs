e09fb8a9667d454cbe646c5b2c85f02887505f73
Kishan Sudani <48625369+kishansudani@users.noreply.github.com>
2024-05-28 20:52:58 +0530

    ether-rs-project

--- ether-rs-projects/communication/src/eth_rpc/mod.rs
+++ ether-rs-projects/communication/src/eth_rpc/mod.rs
@@ -1,20 +1,57 @@
-use ethers::prelude::*;
-use std::error::Error;
+use ethers::{
+    middleware::{
+        gas_escalator::{Frequency, GasEscalatorMiddleware, GeometricGasPrice},
+        gas_oracle::{GasNow, GasOracleMiddleware},
+        MiddlewareBuilder, NonceManagerMiddleware, SignerMiddleware,
+    },
+    prelude::*,
+};
+use gas_oracle::ProviderOracle;
+use std::{env, error::Error};
 
 const RPC: &str = "https://eth.llamarpc.com";
 
 pub struct Ethereum_client {
     provider: Provider<Http>,
+    client: Option<SignerMiddleware<Provider<Http>, LocalWallet>>,
 }
 
 impl Ethereum_client {
-    pub async fn new() -> Result<Ethereum_client, Box<dyn Error>> {
-        Ok(Ethereum_client {
-            provider: Provider::<Http>::try_from(RPC)?,
+    pub fn new() -> Result<Ethereum_client, Box<dyn Error>> {
+        let provider: Provider<Http> = Provider::<Http>::try_from(RPC)?;
+
+        Ok(Self {
+            provider,
+            client: None,
         })
     }
 
-    pub async fn get_block_number(&self) -> Result<U64, Box<dyn Error>> {
+    pub async fn set_client(&mut self, p_key: String) -> Result<(), Box<dyn Error>> {
+        let wallet: LocalWallet = p_key.parse::<LocalWallet>()?;
+        let address = wallet.address();
+        let gas_oracle = GasNow::new();
+
+        // let check: NonceManagerMiddleware<
+        //     SignerMiddleware<GasOracleMiddleware<Provider<Http>, GasNow>, LocalWallet
+        // > = Provider::<Http>::try_from(RPC)
+        //     .unwrap()
+        //     .gas_oracle(gas_oracle)
+        //     .with_signer(wallet.clone())
+        //     .nonce_manager(address);
+
+        let value = SignerMiddleware::new(self.provider.clone(), wallet);
+        self.client = Some(value);
+
+        Ok(())
+    }
+
+    pub fn create_tx(&self, to: H160, value: U256) -> Result<(), Box<dyn Error>> {
+        let tx = TransactionRequest::new().to(to).value(value);
+        println!("TX RAW IS {tx:?}");
+        Ok(())
+    }
+
+    pub async fn get_block_count(&self) -> Result<U64, Box<dyn Error>> {
         Ok(self.provider.get_block_number().await?)
     }
 
@@ -22,7 +59,45 @@ impl Ethereum_client {
         Ok(self.provider.get_chainid().await?)
     }
 
-    pub async fn get_mempool(&self) -> Result<TxpoolContent, Box<dyn Error>> {
+    pub async fn get_tx_pool(&self) -> Result<TxpoolContent, Box<dyn Error>> {
         Ok(self.provider.txpool_content().await?)
+    }
+
+    pub async fn get_block(&self, block_number: u64) -> Result<Block<H256>, Box<dyn Error>> {
+        let block = self.provider.get_block(block_number).await?;
+
+        let result = match block {
+            Some(value) => Ok(value),
+            None => Err(()),
+        };
+
+        Ok(result.unwrap())
+    }
+
+    pub async fn get_account_balance(&self, from: Address) -> Result<U256, Box<dyn Error>> {
+        Ok(self.provider.get_balance(from, None).await?)
+    }
+
+    pub async fn send_coin(
+        &self,
+        from: Address,
+        to: Address,
+        value: U256,
+    ) -> Result<Bytes, Box<dyn Error>> {
+        let tx = TransactionRequest::default()
+            .from(from)
+            .to(to)
+            .value(value)
+            .into();
+        let result = self.provider.call_raw(&tx).await?;
+
+        Ok(result)
+    }
+
+    pub async fn get_gas_price_oracle(&self) -> Result<U256, Box<dyn Error>> {
+        let oracle = ProviderOracle::new(self.provider.clone());
+
+        let price: U256 = oracle.fetch().await?;
+        Ok(price)
     }
 }
--- ether-rs-projects/communication/src/main.rs
+++ ether-rs-projects/communication/src/main.rs
@@ -1,14 +1,26 @@
 // connect with ethereum node
 mod eth_rpc;
 
+use dotenv::dotenv;
 use eth_rpc::Ethereum_client;
 use ethers::prelude::*;
+use ethers::{types::H160, utils::parse_ether};
 use std::error::Error;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let provider = Ethereum_client::new().await?;
-    
+async fn main() {
+    dotenv().ok();
 
-    Ok(())
+    let _: String = std::env::var("SIGNER_PRIVET_KEY").expect("Failed to read signer privet key");
+
+    let test = Ethereum_client::new().unwrap();
+
+    let to_adr: H160 = "0x000000000000000000000000000000000000dead"
+        .parse()
+        .unwrap();
+
+    let val = parse_ether(1u64).unwrap();
+
+    // let _ = test.get_block(10).await;
+    let _ = test.create_tx(to_adr, val).unwrap();
 }
