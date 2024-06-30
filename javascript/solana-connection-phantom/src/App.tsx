89b966daa1a8a57ae7ad634cf6428b35f1b0895d
Kishan Sudani <48625369+kishansudani@users.noreply.github.com>
2024-06-30 13:15:07 +0530

    solana phantom wallet connect

---
+++ javascript/solana-connection-phantom/src/App.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import './App.css';
+import {
+  PublicKey,
+  Transaction,
+} from "@solana/web3.js";
+import {useEffect , useState } from "react";
+import './App.css'
+
+type DisplayEncoding = "utf8" | "hex";
+
+type PhantomEvent = "disconnect" | "connect" | "accountChanged";
+type PhantomRequestMethod =
+  | "connect"
+  | "disconnect"
+  | "signTransaction"
+  | "signAllTransactions"
+  | "signMessage";
+
+interface ConnectOpts {
+  onlyIfTrusted: boolean;
+}
+
+interface PhantomProvider {
+  publicKey: PublicKey | null;
+  isConnected: boolean | null;
+  signTransaction: (transaction: Transaction) => Promise<Transaction>;
+  signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
+  signMessage: (
+    message: Uint8Array | string,
+    display?: DisplayEncoding
+  ) => Promise<any>;
+  connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
+  disconnect: () => Promise<void>;
+  on: (event: PhantomEvent, handler: (args: any) => void) => void;
+  request: (method: PhantomRequestMethod, params: any) => Promise<unknown>;
+}
+
+ const getProvider = (): PhantomProvider | undefined => {
+  if ("solana" in window) {
+    // @ts-ignore
+    const provider = window.solana as any;
+    if (provider.isPhantom) return provider as PhantomProvider;
+  }
+};
+
+export default function App() {
+  const [provider, setProvider] = useState<PhantomProvider | undefined>(
+    undefined
+  );
+
+  const [walletKey, setWalletKey] = useState<PhantomProvider | undefined>(
+  undefined
+  );
+
+  useEffect(() => {
+	  const provider = getProvider();
+
+	  if (provider) setProvider(provider);
+	  else setProvider(undefined);
+  }, []);
+
+  const connectWallet = async () => {
+    // @ts-ignore
+    const { solana } = window;
+
+    if (solana) {
+      try {
+        const response = await solana.connect();
+        console.log('wallet account ', response.publicKey.toString());
+        setWalletKey(response.publicKey.toString());
+      } catch (err) {
+      // { code: 4001, message: 'User rejected the request.' }
+      }
+    }
+  };
+
+	// HTML code for the app
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Connect to Phantom Wallet</h2>
+      {provider && !walletKey && (
+      <button
+        style={{
+          fontSize: "16px",
+          padding: "15px",
+          fontWeight: "bold",
+          borderRadius: "5px",
+        }}
+        onClick={connectWallet}
+      >
+        Connect Wallet
+      </button>
+        )}
+        {provider && walletKey && <p>Connected account</p> }
+
+        {!provider && (
+          <p>
+            No provider found. Install{" "}
+            <a href="https://phantom.app/">Phantom Browser extension</a>
+          </p>
+        )}
+        </header>
+    </div>
+  );
+}
